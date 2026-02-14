ebe601deb1185d007db159d8a85321cd0406e304
aadisinha1814 <adityasinhaaa2005@gmail.com>
2026-02-14 11:07:39 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    // Agar upar wala step kaam na kare, toh yahan tailwindcss() add kar sakte hain
+  ],
+})
